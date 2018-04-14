ed6752c1d57044d8b2b90b0e14267d1918262d54
franiglesias <fran.iglesias@holaluz.com>
2018-04-14 16:28:02 +0200

    Add RandomSyllableGenerator

--- src/RandomSyllableGenerator.php
+++ src/RandomSyllableGenerator.php
@@ -6,13 +6,64 @@ namespace TalkingBit\Readable;
 class RandomSyllableGenerator implements RandomGenerator
 {
     private const VOWEL_GROUP = [
-        'a', 'e', 'i', 'o', 'u',
-        'ai', 'au',
-        'ei', 'eu',
-        'ia', 'ie', 'io', 'iu',
-        'oi', 'ou',
-        'ua', 'ue', 'ui', 'uo'
+        'a',
+        'e',
+        'i',
+        'o',
+        'u',
+        'ai',
+        'au',
+        'ei',
+        'eu',
+        'ia',
+        'ie',
+        'io',
+        'iu',
+        'oi',
+        'ou',
+        'ua',
+        'ue',
+        'ui',
+        'uo'
     ];
+
+    private const CONSONANT_GROUP = [
+        'b',
+        'c',
+        'd',
+        'f',
+        'g',
+        'h',
+        'j',
+        'k',
+        'l',
+        'm',
+        'n',
+        'ñ',
+        'p',
+        'q',
+        'r',
+        's',
+        't',
+        'v',
+        'w',
+        'x',
+        'y',
+        'z',
+        'bl',
+        'br',
+        'ch',
+        'cl',
+        'cr',
+        'fl',
+        'fr',
+        'll',
+        'pr',
+        'pl',
+        'tr',
+        ''
+    ];
+
     /** @var RandomEngine */
     private $randomEngine;
 
@@ -23,8 +74,20 @@ class RandomSyllableGenerator implements RandomGenerator
 
     public function generate(): string
     {
+        return $this->pickAConsonant() . $this->pickAVowel();
+    }
+
+    private function pickAVowel(): string
+    {
         $pick = $this->randomEngine->pickIntegerBetween(0, count(self::VOWEL_GROUP) - 1);
 
         return self::VOWEL_GROUP[$pick];
+    }
+
+    private function pickAConsonant(): string
+    {
+        $pick = $this->randomEngine->pickIntegerBetween(0, count(self::CONSONANT_GROUP));
+
+        return self::CONSONANT_GROUP[$pick];
     }
 }
--- tests/TalkingBit/Readable/RandomSyllableGeneratorTest.php
+++ tests/TalkingBit/Readable/RandomSyllableGeneratorTest.php
@@ -10,18 +10,35 @@ use TalkingBit\Readable\RandomSyllableGenerator;
 class RandomSyllableGeneratorTest extends TestCase
 {
 
-    const VOWEL_GROUP_PATTERN = '/[aeiou]|ai|au|ei|eu|ia|ie|io|iu|oi|ou|ua|ue|ui|uo/';
+    const VOWEL_GROUP_PATTERN = '[aeiou]|ai|au|ei|eu|ia|ie|io|iu|oi|ou|ua|ue|ui|uo';
+    const CONSONANT_GROUP_PATTERN = '[^aeiou]|bl|br|ch|cl|cr|fl|fr|ll|pr|pl|tr';
 
     public function testASyllableHasOneVowelGroup()
     {
         $engineProphecy = $this->prophesize(RandomEngine::class);
         $engineProphecy->pickIntegerBetween(0, 18)->willReturn(4);
+        $engineProphecy->pickIntegerBetween(0, 33)->willReturn(0);
         $generator = new RandomSyllableGenerator($engineProphecy->reveal());
         $this->assertValidVowelGroup($generator->generate());
     }
 
+    public function testASyllableCanStartWithOneConsonantGroup()
+    {
+        $engineProphecy = $this->prophesize(RandomEngine::class);
+        $engineProphecy->pickIntegerBetween(0, 18)->willReturn(0);
+        $engineProphecy->pickIntegerBetween(0, 33)->willReturn(0);
+        $generator = new RandomSyllableGenerator($engineProphecy->reveal());
+        $this->assertStartsWithConsonantGroup($generator->generate());
+    }
+
     public function assertValidVowelGroup(string $syllable): void
     {
-        $this->assertRegExp(self::VOWEL_GROUP_PATTERN, $syllable);
+        $this->assertRegExp(sprintf('/%s/', self::VOWEL_GROUP_PATTERN), $syllable);
+    }
+
+    private function assertStartsWithConsonantGroup(string $syllable): void
+    {
+        $pattern = sprintf('/^(%s)%s/', self::CONSONANT_GROUP_PATTERN, self::VOWEL_GROUP_PATTERN);
+        $this->assertRegExp($pattern, $syllable);
     }
 }
